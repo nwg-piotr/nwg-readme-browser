f5f72d368ad25d9fa952eaed300c58350ae81864
piotr <nwg.piotr@gmail.com>
2024-01-30 02:01:32 +0100

    bump to 0.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(f_name):
 
 setup(
     name='nwg-readme-browser',
-    version='0.1.2',
+    version='0.1.3',
     description='nwg-shell README browser',
     packages=find_packages(),
     include_package_data=True,
