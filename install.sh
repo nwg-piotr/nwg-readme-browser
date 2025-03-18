567a14874edfa2b71edfc85a2afe0ab8baa96431
piotr <nwg.piotr@gmail.com>
2025-03-18 01:01:30 +0100

    improve install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,37 @@
 #!/usr/bin/env bash
 
-python3 setup.py install --optimize=1
+# Before running this script, make sure you have python-build, python-installer,
+# python-wheel and python-setuptools installed.
 
-# 'coreutils' package required
-install -Dm 644 -t "/usr/share/pixmaps" nwg-readme-browser.svg
-install -Dm 644 -t "/usr/share/applications" nwg-readme-browser.desktop
-install -Dm 644 -t "/usr/share/licenses/nwg-readme-browser" LICENSE
-install -Dm 644 -t "/usr/share/doc/nwg-readme-browser" README.md
+PROGRAM_NAME="nwg-readme-browser"
+MODULE_NAME="nwg_readme_browser"
+SITE_PACKAGES="$(python3 -c "import sysconfig; print(sysconfig.get_paths()['purelib'])")"
+PATTERN="$SITE_PACKAGES/$MODULE_NAME*"
+
+# Remove from site_packages
+for path in $PATTERN; do
+    if [ -e "$path" ]; then
+        echo "Removing $path"
+        rm -r "$path"
+    fi
+done
+
+[ -d "./dist" ] && rm -rf ./dist
+
+# Remove launcher scripts
+filenames=("/usr/bin/nwg-readme-browser")
+
+for filename in "${filenames[@]}"; do
+  rm -f "$filename"
+  echo "Removing -f $filename"
+done
+
+python -m build --wheel --no-isolation
+
+python -m installer dist/*.whl
+
+install -Dm 644 -t "/usr/share/applications" "$PROGRAM_NAME.desktop"
+install -Dm 644 -t "/usr/share/pixmaps" "$PROGRAM_NAME.svg"
+
+install -Dm 644 -t "/usr/share/licenses/$PROGRAM_NAME" LICENSE
+install -Dm 644 -t "/usr/share/doc/$PROGRAM_NAME" README.md
